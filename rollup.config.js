478e0bf7b325e4df2c4d0e5521a2f0a947d7c961
Владимир <molchanov.vladimir1993@gmail.com>
2021-11-09 02:10:17 +0700

    Задание #2

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,26 @@
+import styles from "rollup-plugin-styles";
+import image from 'rollup-plugin-img';
+import { babel } from '@rollup/plugin-babel';
+import serve from 'rollup-plugin-serve'
+import livereload from 'rollup-plugin-livereload'
+
 export default {
-    input: './src/index.js',
+    input: 'src/index.js',
     output: {
-        file: './bundle/bundle.js',
+        file: 'bundle/index.js',
         format: 'cjs'
-    }
+    },
+    plugins: [
+        babel({ babelHelpers: 'bundled' }),
+        styles(),
+        image({
+            limit: 1048576,  // (1Mb)
+        }),
+        serve({
+            open: true,
+            contentBase: './',
+            port: 8000,
+        }),
+        livereload(),
+    ],
 };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('Hello world!')
+import "../index.css";
+import logo from '../assets/logo.png';
+
+const header = document.createElement('header')
+
+const h1 = document.createElement('h1')
+h1.textContent = 'I love JavaScript'
+
+const img = document.createElement('img')
+img.src = logo
+
+header.append(img)
+header.append(h1)
+
+document.body.append(header)
+
